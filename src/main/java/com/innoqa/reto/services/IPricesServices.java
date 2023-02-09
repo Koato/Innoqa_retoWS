6a13571f94a4021523fe49aa95d95ff2207a5130
Andy Gomez <45979588+Koato@users.noreply.github.com>
2023-02-09 10:51:35 -0500

    desarrollo

--- src/main/java/com/innoqa/reto/services/IPricesServices.java
+++ src/main/java/com/innoqa/reto/services/IPricesServices.java
@@ -1,10 +1,12 @@
 package com.innoqa.reto.services;
 
+import java.util.List;
 import java.util.Optional;
 import com.innoqa.reto.dao.Prices;
 
 public interface IPricesServices {
-	
+	List<Prices> getPrices();
+
 	Optional<Prices> getPrice(String fechaAplicacion, Long idProducto, Long idCadena);
-	
+
 }
--- src/main/java/com/innoqa/reto/services/impl/PricesImpl.java
+++ src/main/java/com/innoqa/reto/services/impl/PricesImpl.java
@@ -2,12 +2,15 @@ package com.innoqa.reto.services.impl;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.innoqa.reto.dao.Brand;
 import com.innoqa.reto.dao.Prices;
 import com.innoqa.reto.repository.IPricesRepository;
 import com.innoqa.reto.services.IPricesServices;
@@ -20,18 +23,26 @@ public class PricesImpl implements IPricesServices {
 
 	@Override
 	public Optional<Prices> getPrice(String fechaAplicacion, Long idProducto, Long idCadena) {
-		Optional<Prices> otc = pricesRepository.findById(1L);
-		if (otc.isPresent()) {
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);
-			LocalDateTime dateTime = LocalDateTime.parse(fechaAplicacion, formatter);
-			Prices prices = otc.get();
-			prices.setStartDate(dateTime);
-			prices.setProductId(idProducto);
-			Brand brand = new Brand();
-			brand.setId(idCadena);
-			prices.setBrandId(brand);
-			return Optional.of(prices);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);
+		LocalDateTime dateTime = LocalDateTime.parse(fechaAplicacion, formatter);
+		List<Prices> prices = getPrices().stream()
+				.filter(x -> dateTime.isAfter(x.getStartDate()) && dateTime.isBefore(x.getEndDate()))
+				.collect(Collectors.toList());
+		if (prices.stream().count() > 1) {
+			int mayor = 0;
+			for (int i = 0; i < prices.size(); i++) {
+				if (prices.get(i).getPriority() > mayor) {
+					mayor = prices.get(i).getPriority();
+				}
+			}
+			return Optional.of(prices.get(mayor));
+		} else {
+			return prices.stream().findFirst();
 		}
-		return Optional.empty();
+	}
+
+	@Override
+	public List<Prices> getPrices() {
+		return StreamSupport.stream(pricesRepository.findAll().spliterator(), false).collect(Collectors.toList());
 	}
 }
